d8b0d7baa847b94975ff4def137799775b5e0052
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-19 00:55:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,14 +1,12 @@
-function forLoop(array){
-  var array = [0,1,2,3,4,5,6,7,8,9,10,11,
-    12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-  for (i = 0; i <= 25; i++) {
+function forLoop(array
+  var array = i
+  for (let i = 0; i <= 25; i++) {
   if(i == 1) {
     console.log('I am 1 strange loop');
   } else {
     console.log('I am ${i} strange loops');
   }
  }
-   return(array)
 }
 
 function whileLoop(n) {
