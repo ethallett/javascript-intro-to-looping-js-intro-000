42d8c9d90e1ae9054a347ec6d79e53a445bf124d
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-19 02:00:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,13 +16,13 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(num) {
+  var i = 0
   function incrementVariable() {
   i = i + 1;
   return i;
 }
   do { console.log("I run once regardless")
 } while (incrementVariable() < num) {
-    i = i + 1;
-    return i
+
   }
 }
