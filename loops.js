a60fb38cc1df442193a2d25239db2547326eec52
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-19 01:54:17 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,12 +8,12 @@ function forLoop(array) {
 }  return array;
 }
 
-// //function whileLoop(n) {
-//   while (n > 0) {
-//    console.log(n--);
-//  }
-//    return 'done'
-// }
+function whileLoop(n) {
+  while (n > 0) {
+   console.log(n--);
+ }
+   return 'done'
+}
 //
 // function doWhileLoop(num) {
 //   do { console.log("I run once regardless")
